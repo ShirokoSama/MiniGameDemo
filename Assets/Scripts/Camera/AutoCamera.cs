12c0782226367cfd5a6c4ca8eb2151af1cc66216
包纸 <141250108@smail.nju.edu.cn>
2018-06-18 23:13:49 +0800

    啊为什么断开连接

--- Assets/Scripts/Camera/AutoCamera.cs
+++ Assets/Scripts/Camera/AutoCamera.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-namespace AutoCamera
+namespace Camera
 {
 
     public class AutoCamera : MonoBehaviour
---
+++ Assets/Scripts/Character/CharacterContoller.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Character
+{
+    public class CharacterContoller : MonoBehaviour
+    {
+
+        // Use this for initialization
+        void Start()
+        {
+            if (Input.GetMouseButtonDown(0))
+                return;
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
