d4de0663df02938a9cd72907a7db96f9d14c150d
NAiveD <nice-die@live.com>
2018-06-19 21:26:40 +0800

    把部分UI加了上去

---
+++ Assets/Scripts/UIContact/PauseButtonListener.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseButtonListener : MonoBehaviour {
+
+	public void ButtonClick()
+    {
+        Debug.Log("Button clicked!");
+    }
+}
