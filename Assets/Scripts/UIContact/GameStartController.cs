3057866ec771de64c0b2dae5f2f29bf3afeeb3db
frankhjwx <frankhjwx@gmail.com>
2018-07-26 14:12:57 +0800

    改了一丁点动画bug

--- Assets/Scripts/UIContact/GameStartController.cs
+++ Assets/Scripts/UIContact/GameStartController.cs
@@ -10,46 +10,53 @@ public class GameStartController : MonoBehaviour
 
     private float TargetAlpha = 1.0f;
     private float duration = 1.0f;
+    private bool StartFade = false;
     
     // Use this for initialization
     void Start ()
 	{
 	    canvasGroup = this.GetComponent<CanvasGroup>();
-	}
+	    StartFade = false;
+    }
 
     public void gameStart()
     {
         TargetAlpha = 0.0f;
+        StartFade = true;
     }
 	
 	// Update is called once per frame
 	void Update () {
-	    if (this.canvasGroup.alpha != TargetAlpha)
-	    {
-	        if (Mathf.Abs(TargetAlpha - this.canvasGroup.alpha) <= 0.01f)
-	            this.canvasGroup.alpha = TargetAlpha;
-            else { 
-	            this.canvasGroup.alpha -= 0.03f;
-                
+        if (StartFade) { 
+	        if (this.canvasGroup.alpha != TargetAlpha)
+	        {
+	            if (Mathf.Abs(TargetAlpha - this.canvasGroup.alpha) <= 0.01f)
+	                this.canvasGroup.alpha = TargetAlpha;
+                else { 
+	                this.canvasGroup.alpha -= 0.03f;
+                    
+	            }
+            }
+	        foreach (var shader in gaussShaders)
+	        {
+	            if (shader.enabled)
+	                if (shader.BlurSpreadSize != 0.0f)
+	                {
+                        Debug.Log(shader.BlurSpreadSize);
+	                    if (Mathf.Abs(shader.BlurSpreadSize - 0.0f) <= 0.01f)
+	                        shader.UpdateBlurSpread(0.0f);
+                        else
+	                        shader.UpdateBlurSpread(shader.BlurSpreadSize - 0.03f);
+
+	                    Debug.Log(shader.BlurSpreadSize);
+                    }
+	                else
+	                {
+	                    shader.enabled = false;
+	                    StartFade = false;
+	                    this.enabled = false;
+	                }
 	        }
         }
-	    foreach (var shader in gaussShaders)
-	    {
-	        if (shader.enabled)
-	            if (shader.BlurSpreadSize != 0.0f)
-	            {
-                    Debug.Log(shader.BlurSpreadSize);
-	                if (Mathf.Abs(shader.BlurSpreadSize - 0.0f) <= 0.01f)
-	                    shader.UpdateBlurSpread(0.0f);
-                    else
-	                    shader.UpdateBlurSpread(shader.BlurSpreadSize - 0.03f);
-
-	                Debug.Log(shader.BlurSpreadSize);
-                }
-	            else
-	            {
-	                shader.enabled = false;
-	            }
-	    }
     }
 }
